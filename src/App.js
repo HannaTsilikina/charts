42a282957955fb8b3d1c3c362556f9286a9449cf
HannaTsilikina <https://github.com/HannaTsilikina/Practicum17.git>
2023-12-15 23:33:22 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 9],
+  ["Eat", 2],
+  ["Commute", 3],
+  ["Learning", 4],
+  ["Sleep", 6],
+];
+
+export const options = {
+  title: "My Daily Activities",
+  is3D: true,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width="100%"
+      height="400px"
+    />
   );
 }
 
