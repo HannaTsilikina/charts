7d4487723353b41e70f459c5353ee73a16433534
HannaTsilikina <https://github.com/HannaTsilikina/Practicum17.git>
2024-02-21 21:15:58 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import "./App.css";
-import { Chart } from "react-google-charts";
-
-export const data = [
-  ["Task", "Hours per Day"],
-  ["Work", 9],
-  ["Eat", 2],
-  ["Commute", 3],
-  ["Learning", 4],
-  ["Sleep", 6],
-];
-
-export const options = {
-  title: "My Daily Activities",
-  is3D: true,
-};
+import React from "react";
+import Form from "./Form";
 
 function App() {
   return (
-    <Chart
-      chartType="PieChart"
-      data={data}
-      options={options}
-      width="100%"
-      height="400px"
-    />
+    <>
+      <Form />
+    </>
   );
 }
 
